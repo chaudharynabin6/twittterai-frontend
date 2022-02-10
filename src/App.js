e76bd3c237739405c068b7025500f244f56b1c94
chaudharynabin6 <chaudharynabin6@gmail.com>
2022-02-10 22:22:09 +0545

    search page : html and css scratch up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+
+// icons
+import arrow_back from "./assets/icons/arrow_back.svg";
+import search from "./assets/icons/search.svg";
+import cancel from "./assets/icons/cancel.svg";
+import more_horiz from "./assets/icons/more_horiz.svg";
+
+// images
+import elon_musk from "./assets/images/elon_musk.jpg";
+import jeff_bezos from "./assets/images/jeff_bezos.jpg";
+
+// components
+
+import SearchPage from "./pages/SearchPage/SearchPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchPage></SearchPage>
     </div>
   );
 }
