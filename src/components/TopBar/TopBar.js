1b47ff6b305c50e9204f39d30a0739cc7b964d2b
chaudharynabin6 <chaudharynabin6@gmail.com>
2022-02-11 18:55:40 +0545

    topbar : html and css scratchup

---
+++ src/components/TopBar/TopBar.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./TopBar.scss";
+// icons
+import home_breadcrumbs from "./../../assets/icons/home_breadcrumbs.svg";
+import notification from "./../../assets/icons/notification.svg";
+
+const TopBar = () => {
+  return (
+    <>
+      <div className="topbar">
+        <div className="topbar__page-info">
+          <div className="topbar__page">
+            <div className="topbar__page-link">
+              <img
+                className="topbar__home-icon"
+                src={home_breadcrumbs}
+                alt=""
+              />
+              <label className="topbar__page-location">
+                / pages / Analytics
+              </label>
+            </div>
+            <label className="topbar__page-title">Analytics</label>
+          </div>
+        </div>
+        <div className="topbar__notification">
+          <img
+            src={notification}
+            alt=""
+            id="notification-icon"
+            className="topbar__notification-icon"
+          />
+          <label
+            htmlFor="notification-icon"
+            className="topbar__notification-label"
+          >
+            15
+          </label>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TopBar;
