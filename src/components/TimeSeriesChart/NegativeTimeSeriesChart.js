5c6021f2a708072d432cbabc8f88aece20c98f29
chaudharynabin6 <chaudharynabin6@gmail.com>
2022-02-27 21:19:36 +0545

    TimeSeriesChart : Adding New Components
    * NegativeTimeSeriesChart
    * PositiveTimeSeriesChart

---
+++ src/components/TimeSeriesChart/NegativeTimeSeriesChart.js
@@ -0,0 +1,96 @@
+import React from "react";
+
+import {
+  ResponsiveContainer,
+  AreaChart,
+  XAxis,
+  YAxis,
+  Area,
+  Tooltip,
+  CartesianGrid,
+} from "recharts";
+import { format, parseISO, subDays } from "date-fns";
+
+// scss
+import "./TimeSeriesChart.scss";
+import { useDashboardPageContext } from "../../pages/DashboardPage/Dashboard.context";
+
+function CustomTooltip({ active, payload, label }) {
+  if (active) {
+    return (
+      <div className="tooltip">
+        <h4>{format(parseISO(label), "eeee, d MMM, yyyy")}</h4>
+        {/* <p>Positive {payload[0].value.toFixed(2)} tweets</p> */}
+        <p>Negative {payload[0].value.toFixed(2)} tweets</p>
+      </div>
+    );
+  }
+  return null;
+}
+
+const NegativeTimeSeriesChart = () => {
+  let { time_series_summary } = useDashboardPageContext();
+
+  time_series_summary.sort((a, b) => {
+    return a.position - b.position;
+  });
+
+  time_series_summary = time_series_summary.map((item) => {
+    const { date, positive, negative } = item;
+    return {
+      date,
+      //   positive,
+      negative,
+    };
+  });
+
+  return (
+    <>
+      <ResponsiveContainer
+        width="100%"
+        height="100%"
+        className="chart-positive-negative-container"
+      >
+        <AreaChart data={time_series_summary}>
+          <defs>
+            <linearGradient id="color" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="0%" stopColor="#2451B7" stopOpacity={0.4} />
+              <stop offset="75%" stopColor="#2451B7" stopOpacity={0.05} />
+            </linearGradient>
+          </defs>
+
+          {/* <Area dataKey="positive" stroke="#2451B7" fill="url(#color)" /> */}
+          <Area dataKey="negative" stroke="#24ffB7" fill="url(#color)" />
+
+          <XAxis
+            dataKey="date"
+            axisLine={false}
+            tickLine={false}
+            tickFormatter={(str) => {
+              const date = parseISO(str);
+              if (date.getDate() % 7 === 0) {
+                return format(date, "MMM, d");
+              }
+              return "";
+            }}
+          />
+
+          {/* <YAxis
+            datakey="positive"
+            axisLine={false}
+            tickLine={false}
+            tickCount={8}
+            // tickFormatter={(number) => `${number.toFixed(2)}`}
+            tickFormatter={(number) => ``}
+          /> */}
+
+          <Tooltip content={<CustomTooltip />} />
+
+          <CartesianGrid opacity={0.1} vertical={false} />
+        </AreaChart>
+      </ResponsiveContainer>
+    </>
+  );
+};
+
+export default NegativeTimeSeriesChart;
---
+++ src/components/TimeSeriesChart/index.js
@@ -0,0 +1,5 @@
+import NegativeTimeSeriesChart from "./NegativeTimeSeriesChart";
+import PositiveTimeSeriesChart from "./PostiveTimeSeriesChart";
+import TimeSeriesChart from "./TimeSeriesChart";
+
+export { NegativeTimeSeriesChart, PositiveTimeSeriesChart, TimeSeriesChart };
