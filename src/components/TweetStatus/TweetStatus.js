45bc2a972a23e35e9ddeb008d3de1b4e826eebbf
chaudharynabin6 <chaudharynabin6@gmail.com>
2022-03-02 18:12:56 +0545

    TwitterStatus : Update
    * using actual data for instead of dummy data

--- src/components/TweetStatus/TweetStatus.js
+++ src/components/TweetStatus/TweetStatus.js
@@ -3,12 +3,15 @@ import React from "react";
 // scss
 import "./TweetStatus.scss";
 // icons
-import positive from "./../../assets/icons/positive.svg";
+import positiveIcon from "./../../assets/icons/positive.svg";
+import { useDashboardPageContext } from "../../pages/DashboardPage/Dashboard.context";
 
+let negativeIcon = positiveIcon;
+let fetchedIcon = positiveIcon;
+let analysingIcon = positiveIcon;
 const TweetStatus = () => {
-  let negative = positive;
-  let fetched = positive;
-  let analysing = positive;
+  const { total_summary } = useDashboardPageContext();
+  const { positive, negative, total, total_fetched } = total_summary;
   return (
     <>
       <section className="tweetstatus-container">
@@ -17,7 +20,7 @@ const TweetStatus = () => {
           <div className="tweetstatus__positive">
             <div className="tweetstatus__positive-icon">
               <img
-                src={positive}
+                src={positiveIcon}
                 alt=""
                 className="tweetstatus__positive-icon__image"
               />
@@ -25,7 +28,7 @@ const TweetStatus = () => {
             <div className="tweetstatus__positive-detail">
               <div className="tweetstatus__positive-info">
                 <span className="tweetstatus__positive-heading">Positive</span>
-                <span className="tweetstatus__positive-number">200</span>
+                <span className="tweetstatus__positive-number">{positive}</span>
               </div>
               <div className="tweetstatus__positive-change">
                 <samp className="tweetstatus__positive-percent">+55%</samp> than
@@ -37,7 +40,7 @@ const TweetStatus = () => {
           <div className="tweetstatus__negative">
             <div className="tweetstatus__negative-icon">
               <img
-                src={negative}
+                src={negativeIcon}
                 alt=""
                 className="tweetstatus__negative-icon__image"
               />
@@ -45,7 +48,7 @@ const TweetStatus = () => {
             <div className="tweetstatus__negative-detail">
               <div className="tweetstatus__negative-info">
                 <span className="tweetstatus__negative-heading">negative</span>
-                <span className="tweetstatus__negative-number">200</span>
+                <span className="tweetstatus__negative-number">{negative}</span>
               </div>
               <div className="tweetstatus__negative-change">
                 <samp className="tweetstatus__negative-percent">+55%</samp> than
@@ -57,7 +60,7 @@ const TweetStatus = () => {
           <div className="tweetstatus__fetched">
             <div className="tweetstatus__fetched-icon">
               <img
-                src={fetched}
+                src={fetchedIcon}
                 alt=""
                 className="tweetstatus__fetched-icon__image"
               />
@@ -65,7 +68,9 @@ const TweetStatus = () => {
             <div className="tweetstatus__fetched-detail">
               <div className="tweetstatus__fetched-info">
                 <span className="tweetstatus__fetched-heading">fetched</span>
-                <span className="tweetstatus__fetched-number">200</span>
+                <span className="tweetstatus__fetched-number">
+                  {total_fetched}
+                </span>
               </div>
               <div className="tweetstatus__fetched-change">
                 <samp className="tweetstatus__fetched-percent">+55%</samp> than
@@ -77,17 +82,15 @@ const TweetStatus = () => {
           <div className="tweetstatus__analysing">
             <div className="tweetstatus__analysing-icon">
               <img
-                src={analysing}
+                src={analysingIcon}
                 alt=""
                 className="tweetstatus__analysing-icon__image"
               />
             </div>
             <div className="tweetstatus__analysing-detail">
               <div className="tweetstatus__analysing-info">
-                <span className="tweetstatus__analysing-heading">
-                  analysing
-                </span>
-                <span className="tweetstatus__analysing-number">200</span>
+                <span className="tweetstatus__analysing-heading">analysed</span>
+                <span className="tweetstatus__analysing-number">{total}</span>
               </div>
               <div className="tweetstatus__analysing-change">
                 <samp className="tweetstatus__analysing-percent">+55%</samp>{" "}
